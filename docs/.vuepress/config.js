7352a8bd33d01cdc761008be95a957579df52ab0
AnyFork <zhangtao19910722>
2022-02-17 00:25:09 +0800

    删除多余的配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,10 +13,8 @@ module.exports = {
     ['link', { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['link', { rel: 'stylesheet', type: 'text/css', href: '/css/demo.css' }],
     ['script', { language: 'javascript', type: 'text/javascript', src: '/js/jquery.min.js' }],
-    ['script', { language: 'javascript', type: 'text/javascript', src: '/js/flux.min.js' }],
-    ['script', { language: 'javascript', type: 'text/javascript', src: '/js/global.js' }]
+    ['script', { language: 'javascript', type: 'text/javascript', src: '/js/flux.min.js' }]
   ],
   //修改默认的语言
   locales: {
