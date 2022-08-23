0280aae8a00ff48e4e9b49de04f913e6d2247180
anyfork <874957743@qq.com>
2022-08-23 22:28:26 +0800

    🐞 fix: fix package.json

--- .vuepress/.temp/internal/layoutComponents.js
+++
@@ -1,6 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export const layoutComponents = {
-  "404": defineAsyncComponent(() => import("G:/前端项目/vuepress博客系统/blog-docs-2.x/node_modules/@vuepress/theme-default/lib/client/layouts/404.vue")),
-  "Layout": defineAsyncComponent(() => import("G:/前端项目/vuepress博客系统/blog-docs-2.x/node_modules/@vuepress/theme-default/lib/client/layouts/Layout.vue")),
-}
